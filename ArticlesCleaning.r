b325b7fc44007692d7c948e0798b21e8b4d25bd8
Daniel <daniel-oconnor96@hotmail.com>
2020-02-29 19:56:19 +0000

    News Article text-cleaning script Need to add function to concat filtered articles which mention Tesla/Elon Musk Also need to export the CSV & test data

--- ArticlesCleaning.r
+++ ArticlesCleaning.r
@@ -7,10 +7,48 @@ library(textreg)
 #Package for str_replace
 install.packages("stringr")
 library(stringr)
+install.packages("dplyr")
+library(dplyr)
 #################################
 
-#problem here is this file is only articles 1. 2 more files. These need to be concatenated
-#need to decide on how much of the article data will be used
-#if issue - mention to Divyaa
-ArticlesRaw = read.csv(file.choose(), stringsAsFactors = FALSE)
+ArticlesRaw3 = read.csv(file.choose(), stringsAsFactors = FALSE)
+
+#filter by articles about Tesla/Musk
+filteredArticles1 <- dplyr::filter(ArticlesRaw1, grepl('Tesla|Elon Musk', content))
+filteredArticles2 <- dplyr::filter(ArticlesRaw2, grepl('Tesla|Elon Musk', content))
+filteredArticles3 <- dplyr::filter(ArticlesRaw3, grepl('Tesla|Elon Musk', content))
+
+###### before running below functions - combine the 3 article objects
+
+#store article content column column in var
+Column    <- filteredArticles1$content
+
+#convert content column to corpus for tm use
+Corpus    <- VCorpus(VectorSource(Column))
+
+#convert upper case letters to lower
+Corpus    <- tm_map(Corpus, content_transformer(tolower))
+#stopwords removal 
+Corpus    <- tm_map(Corpus, removeWords, stopwords(kind='en'))
+#remove white space
+Corpus    <- tm_map(Corpus, stripWhitespace)
+#remove numbers
+Corpus    <- tm_map(Corpus, content_transformer(removeNumbers))
+#remove punctuation
+Corpus    <- tm_map(Corpus, content_transformer(removePunctuation))
+
+#convert the corpus back to character vector
+ArticleContentVector <- convert.tm.to.character(Corpus)
+
+#Replace TweetContent column with the clean version
+filteredArticles1$content <- ArticleContentVector
+
+
+
+
+############# CSV export ###############
+
+
+
+
 
