e4164d5869693f25c7e161633bbd364c4f0afdb6
Daniel <daniel-oconnor96@hotmail.com>
2020-03-20 20:56:15 +0000

    -Read in factStock table from DW -merged stock & Tweet dataframes on date -Pearson correlation coefficent plot added

--- TweetSentimentAnalysis.r
+++ TweetSentimentAnalysis.r
@@ -3,13 +3,15 @@ install.packages("RCurl")
 install.packages("wordcloud")
 install.packages("odbc")
 install.packages("sqldf")
+install.packages("ggpubr")
 library(sentimentr)
-library(ggplot2)
 library(RColorBrewer)
 library(RCurl)
 library(wordcloud)
 library(tm)
 library(sqldf)
+library(DBI)
+library(ggpubr)
 
 #Connect R to SQL Server
 library(odbc)
@@ -19,6 +21,9 @@ con <- dbConnect(odbc(),
                  Database = "StockPriceProject",
                  Trusted_Connection = "yes",
                  sep =";")
+
+#Read fact table as DF
+factStock <- dbReadTable(con, "factStock")
 
 #Sentiment function
 sentiment <- sentiment_by(TweetsClean$TweetContent)
@@ -62,11 +67,29 @@ TweetsCorpus  <- tm_map(TweetsCorpus, removeWords, c("elon","musk", "tesla"))
 wordcloud(TweetsCorpus, max.words=150, min.freq=10, random.order=F, colors=brewer.pal(8, "Dark2"))
 
 ## Pearson's correlation coefficient ##
-#Create df by merging stocks & Tweets on date
+#set date vars
+StockDate <- factStock$StockDate
+TweetDate <- TweetsClean$TweetDate
+
+#Join Tweets & Stocks on date
+CorDF <- merge.data.frame(TweetsClean, factStock, by.x = "TweetDate", by.y = "StockDate")
+
+ClosePrice      <- CorDF$Close
+TweetSentiment  <- CorDF$ave_sentiment
+
+cor(ClosePrice, TweetSentiment,  method = "pearson", use = "complete.obs")
+
+#count of tweets by day??? correlation between count of tweets & stock price
+
+#plot correlation
+ggscatter(CorDF, x = "Close", y = "ave_sentiment", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "x title", ylab = "y title")
 
 
-CorDF <- merge.data.frame()
-cor(x, y,  method = "pearson", use = "complete.obs")
+
+
 
 ##################################### 
 
